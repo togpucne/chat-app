f915e7d4c1909a183ae3e2819fa4b08664775e6f
judoittt <your_email@example.com>
2026-05-06 15:29:03 +0700

    Done

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, { family: 4 });
     console.log(`Mongoose connect: ${conn.connection.host}`);
   } catch (error) {
     console.log("Mongoose error: " , error);
