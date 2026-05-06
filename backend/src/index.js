6e50cd0c7527e544c37539680054c111852ecbaf
judoittt <your_email@example.com>
2026-05-06 14:31:52 +0700

    Update project chat

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,9 +8,9 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from './lib/socket.js';
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server started on port: " + PORT);
   connectDB();
 });
