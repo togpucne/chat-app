0d4e98d3825e2c8f79dda51eb571092e868f1c22
judoittt <your_email@example.com>
2026-05-20 19:51:15 +0700

    project chat app

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,9 +10,13 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from './lib/socket.js';
+import path from "path";
+
+
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 
 
@@ -27,6 +31,15 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/ai", aiRoutes);
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 
 server.listen(PORT, () => {
   console.log("Server started on port: " + PORT);
